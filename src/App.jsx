ac6085e08f3f12f7def87dec308a9e19bdccba36
ChanakaRajapaksha <rcprajapaksha123@gmail.com>
2026-02-11 22:51:29 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-2xl text-yellow-500">Mojito Website</h1>
+    </div>
+  )
+};
+
+export default App;
